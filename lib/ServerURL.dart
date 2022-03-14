1387383f02b18bb6fed687206fc420aa0201d08e
LBF-2004 <77555511+LBF-2004@users.noreply.github.com>
2022-03-14 15:30:28 -0700

    ip
    ip

--- lib/ServerURL.dart
+++ lib/ServerURL.dart
@@ -1,6 +1,7 @@
 class ServerURL {
   // static final url = "https://bbc7588d58d9.ngrok.io/";
-  static final url = "http://52.53.220.182:5000/";
-  // static final url = "http://10.0.2.2:5000";
-
+  //static final url = "http://52.53.220.182:5000/";
+  //static final url = "http://10.0.2.2:5000";
+  static final url = "http://3.101.125.22:5000/";
+  //static final url = 'http://192.168.86.106:5000/';
 }
